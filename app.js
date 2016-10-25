eb0e5d95c6f9e6e32c5aa6753c22942410ff5bee
marian27s <maryan27m@gmail.com>
2016-10-25 19:22:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 (function () {
 'use strict'
-angular.module('myFirstApp', [])
+angular.module('NameCalculator', [])
 
-.controller('MyFirstController', function ($scope) {
-	$scope.name = 'Bryant';
+.controller('NameCalculatorController', function ($scope) {
+	$scope.name = "";
+	$scope.totalValue = 0;
+	$scope.displayNumetic = function () {
+		var totalNameValue = calculateNumerForString($scope.name);
+		$scope.totalValue = totalNameValue;
+	};
+	function calculateNumerForString (str) {
+		var totalStringValue = 0;
+		for(var i = 0; i<str.length; i++){
+			totalStringValue+=str.charCodeAt(i);
+		};
+		
+		return totalStringValue;
+	}
+	
 });
 
 
