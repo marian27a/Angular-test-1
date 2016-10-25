754d0fca8f2d37f263dd81f6703ad3447fd14d13
marian27s <maryan27m@gmail.com>
2016-10-25 18:02:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 'use strict'
 angular.module('myFirstApp', []);
 
-.controller('MyFirstController', function () {
-	
+.controller('MyFirstController', function ($scope) {
+	$scope.name = 'Bryant'
 })
 
 
